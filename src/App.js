5b043f56f007bf3c3c499f22dad6097b9412ee6b
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-11-15 00:34:46 +0200

    functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Great from './components/Great';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Great />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Great.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+const Great = ()=> <h1>Hello from functtional component</h1>
+export default Great
