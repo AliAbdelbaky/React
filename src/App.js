486e52dee51d49d067567728cc6888f0477410e4
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-11-15 00:41:53 +0200

    class-statefull

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Welcome from './components/Welcome';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Welcome />
         <a
           className="App-link"
           href="https://reactjs.org"
